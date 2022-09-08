9cf3c0c073f333725923ebcddb4b683352f67dd8
tingxin <friendship-119@163.com>
2022-09-08 06:53:19 +0000

    support concurrent

--- sync_data.py
+++ sync_data.py
@@ -18,7 +18,7 @@ athena_output_cfg = {
 # 目标表表名
 target_table = "demo_prod_yunli_athena_tb"
 # 临时表表名
-source_temp_table = "demo_yunli_athena_tb3"
+source_temp_table = "temp"
 # athena 工作组
 athena_work_group = 'primary'
 
@@ -27,9 +27,35 @@ dynamodb_status_table_key = 'keyetag'
 # ===================================================================
 
 
-def lambda_handler(event, context):
+def query(athena, sql):
+    execution = athena.start_query_execution(
+        QueryString=sql,
+        QueryExecutionContext=athena_data_ctx,
+        ResultConfiguration=athena_output_cfg,
+        WorkGroup=athena_work_group
+    )
+    query_result = False
+    print(f"begin query:\n{sql}")
+    while True:
 
-    print(event)
+        response = athena.get_query_execution(
+            QueryExecutionId=execution['QueryExecutionId'])
+        status = response['QueryExecution']['Status']['State']
+
+        if status == 'SUCCEEDED':
+            # 打印本次查询数据扫描量
+            print(float(response['QueryExecution']['Statistics']
+                        ['DataScannedInBytes'])/1024/1024/1024)
+
+            query_result = True
+            break
+        if status == 'FAILED':
+            print(f"failed to execute sql:{sql}")
+            break
+    return query_result
+
+
+def lambda_handler(event, context):
     keys = [record['body'] for record in event['Records']]
     parts = target_s3.split("/")
 
@@ -60,66 +86,88 @@ def lambda_handler(event, context):
             ukey = urllib.parse.unquote_plus(key, encoding='utf-8')
             items = ukey.split("/")
             file_name = items[-1]
-            target_file = target_key + file_name
+            target_file = target_key + verified_key + "/" + file_name
 
             response = s3.copy_object(
                 CopySource=ukey,
                 Bucket=target_bucket,
                 Key=target_file
             )
-            time.sleep(1)
 
-            create_dt = "-".join(items[-4:-1])
-            business_type = items[-5]
+            process_status = False
+            error = '-'
 
-            # 目标表的create_dt 的数据类型，如果是字符串，需要把 date 感谢
-            sql = f"""
-            insert into {target_table}
-            select json_text, '{business_type}' as business_type, date '{create_dt}' as create_dt
-            from {source_temp_table}
-            """
-            execution = athena.start_query_execution(
-                QueryString=sql,
-                QueryExecutionContext=athena_data_ctx,
-                ResultConfiguration=athena_output_cfg,
-                WorkGroup=athena_work_group
-            )
-            query_result = False
             while True:
-                print("begin query")
-                response = athena.get_query_execution(
-                    QueryExecutionId=execution['QueryExecutionId'])
-                status = response['QueryExecution']['Status']['State']
-
-                if status == 'SUCCEEDED':
-                    # 打印本次查询数据扫描量
-                    print(float(response['QueryExecution']['Statistics']
-                                ['DataScannedInBytes'])/1024/1024/1024)
-
-                    query_result = True
+                # 创建临时表
+                # 这块需要注意调整， 尤其注意 target_s3 的路径，后面是带一个 /
+                etag = verified_key.split("/")[-1]
+                # remove .tar.gz 等后缀
+                raw_file_name = file_name.split(".")[0]
+                source_table = f"{source_temp_table}_{etag}_{raw_file_name}"
+                ddl = f"""
+                CREATE EXTERNAL TABLE `{source_table}`(
+                `json_text` string COMMENT 'from deserializer')
+                ROW FORMAT SERDE 
+                'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe' 
+                STORED AS INPUTFORMAT 
+                'org.apache.hadoop.mapred.TextInputFormat' 
+                OUTPUTFORMAT 
+                'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+                LOCATION
+                '{target_s3}{verified_key}/'
+                """
+                process_status = query(athena, ddl)
+                if not process_status:
+                    error = 'failed to create temp table'
                     break
-                if status == 'FAILED':
-                    print(f"failed to insert table {key}")
+                time.sleep(0.2)
+
+                create_dt = "-".join(items[-4:-1])
+                business_type = items[-5]
+
+                # 目标表的create_dt 的数据类型，如果是字符串，需要把 date 感谢
+                sql = f"""
+                insert into {target_table}
+                select json_text, '{business_type}' as business_type, date '{create_dt}' as create_dt
+                from {source_table}
+                """
+
+                process_status = query(athena, sql)
+                time.sleep(0.2)
+                # 删除已经同步过去的文件
+                o = [{
+                    "Key": target_file
+                },
+                    {
+                    "Key": target_key + verified_key + "/"
+                }
+                ]
+                print(f"begin to delete {o}")
+                d = {
+                    'Objects': o,
+                    'Quiet': True
+                }
+
+                response = s3.delete_objects(
+                    Bucket=target_bucket,
+                    Delete=d
+                )
+                print(f"delete used file {response}")
+                if not process_status:
+                    error = 'failed to insert data from temp table'
                     break
-                time.sleep(1)
 
-            # 删除已经同步过去的文件
-            o = [{
-                "Key": target_file
-            }]
-            print(f"begin to delete {o}")
-            d = {
-                'Objects': o,
-                'Quiet': True
-            }
+                drop = f"""
+                drop table {source_table}
+                """
+                process_status = query(athena, drop)
+                if not process_status:
+                    error = 'failed to drop temp table'
 
-            response = s3.delete_objects(
-                Bucket=target_bucket,
-                Delete=d
-            )
-            print(f"delete used file {response}")
-            status = 'success' if query_result else 'failure'
-            error = '-' if query_result else f'failed to insert table: {key}'
+                # 注意这里的break, 否则就死循环了
+                break
+
+            status = 'success' if process_status else 'failure'
             dynamodb.update_item(
                 TableName=dynamodb_status_table,
                 Key={
